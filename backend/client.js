22fc215adf9da3a5ef87598b18ff738296fe91a5
SamG82 <samg13579@gmail.com>
2023-11-10 17:33:13 -0500

    creating practioner

--- backend/client.js
+++ backend/client.js
@@ -1,10 +1,48 @@
 const axios = require('axios')
 
+
+const fakeNPI = 1172881645
+
 class FHIRClient {
     constructor(base_url, token) {
         this.axios = axios.create({
             baseURL: base_url,
             headers: {'Authorization': `Bearer ${token}`}
+        })
+    }
+
+    createPractioner(firstName, lastName) {
+        const data = {
+            "name": [
+                {
+                    "use": "usual",
+                    "text": `${firstName} ${lastName}`,
+                    "family": lastName,
+                    "given": [
+                        firstName
+                    ]
+                }   
+            ],
+            "identifier": [
+                {
+                    "type": {
+                        "coding": [
+                            {
+                                "system": "http://terminology.hl7.org/CodeSystem/v2-0203",
+                                "code": "NPI" 
+                            }
+                        ]
+                    },
+                    "system": "http://hl7.org/fhir/sid/us-npi",
+                    "value": fakeNPI
+                }
+            ]
+        }
+
+        this.axios.post('/Practitioner', data)
+        .then(response => console.log(response.data))
+        .catch(err => {
+            console.log(err)
         })
     }
 }
--- backend/main.js
+++ backend/main.js
@@ -2,8 +2,8 @@ const FHIRClient = require('./client')
 require('dotenv').config()
 
 const root_url = process.env.ROOT_URL
-const token = process.env.OATH
+const token = process.env.TOKEN
 
 const fhir = new FHIRClient(root_url, token)
 
-
+fhir.createPractioner("Test Javascript", "Javascript test")
