690764d1cd8e58a65bcd4bae66e5d211adde03d6
Kendall Eberly <ke05739@georgiasouthern.edu>
2023-11-11 00:01:00 -0500

    patient

--- backend/users.js
+++ backend/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Practitioner = require('./schemas/practitioner')
+const Patient = require('./schemas/patient')
 const FHIRClient = require('./client')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
@@ -14,6 +15,31 @@ const createJWT = (id, role) => {
         role
     }, process.env.JWT_SECRET)
 }
+
+router.post('/patient/register', async (req, res) => {
+    const potentialPatient = await Patient.findOne({email: req.body.email})
+    if (potentialPatient != null) {
+        return res.status(401).json({'error': 'Email already in use'})
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+            const newPatient = new Patient({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                password: hash,
+                email: req.body.email,
+                familyContact: req.body.familyContact
+            })
+        
+            newPatient.save().then(() => {
+                console.log('created')
+                fhir.createPatient(req.body.firstName, req.body.lastName, req.body.email, req.body.familyContact)
+                res.sendStatus(200)
+            })
+        })
+    })
+})
 
 router.post('/practitioner/register', async (req, res) => {
     const potentialPractitioner = await Practitioner.findOne({email: req.body.email})
