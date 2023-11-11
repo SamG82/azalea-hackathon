a5e1fd068fcc099751e8bfa897ebfe35105180a0
Kendall Eberly <ke05739@georgiasouthern.edu>
2023-11-11 11:04:27 -0500

    getpatientsHTTP

--- backend/main.js
+++ backend/main.js
@@ -26,10 +26,19 @@ async function main() {
     const result = await fhir.getAllPatients()
     console.log(result)
 
+    app.get('/patients', async (req, res) => {
+        const patients = await fhir.getAllPatients();
+        res.json(patients);
+    });
+    
     app.use(cors(corsOptions))
     app.use(cookieParser())
     app.use(express.json())
     app.use('/users', usersRouter)
+    app.get('/patients', async (req, res) => {
+    const patients = await fhir.getAllPatients();
+    res.json(patients);
+});
 
     app.listen(3000, () => {
         console.log('Server started on port 3000')
