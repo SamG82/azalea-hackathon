00a85e77e78f3e15fc559610739b2f37b88aa1df
SamG82 <samg13579@gmail.com>
2023-11-11 11:27:04 -0500

    patient selection

--- backend/main.js
+++ backend/main.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+
 require('dotenv').config()
 
 const FHIRClient = require('./client')
@@ -22,19 +24,29 @@ async function main() {
         credentials: true,
         origin: ['http://localhost:5173']
     }
-    
-    const result = await fhir.getAllPatients()
-    console.log(result)
+    app.use(cookieParser())
+    app.use(cors(corsOptions))
+    app.use(express.json())
+    app.use('/users', usersRouter)
 
     app.get('/patients', async (req, res) => {
+        let payload = {}
+        try {
+            payload = jwt.verify(req.cookies.token, process.env.JWT_SECRET)
+        } catch(err) {
+            console.log(err)
+            return res.sendStatus(401)
+        }
+
+        if (payload.role != 'practitioner') {
+            return res.sendStatus(401)
+        }
+
         const patients = await fhir.getAllPatients();
         res.json(patients);
     });
     
-    app.use(cors(corsOptions))
-    app.use(cookieParser())
-    app.use(express.json())
-    app.use('/users', usersRouter)
+    
 
     app.listen(3000, () => {
         console.log('Server started on port 3000')
